98db04b22374d754336e0508e734032736714261
bagelsfordagels <benurgh@gmail.com>
2026-01-28 12:20:12 -0500

    added csv folder

--- demo.js
+++ demo.js
@@ -1,7 +1,7 @@
 const searchBox = document.getElementById('search-label');
 const regionList = document.getElementById('region-filter');
 const climateInfo = document.getElementById('climate-filter');
-
+MondayMonday
 let allTrees = [];
 let filteredTrees = [];
 
