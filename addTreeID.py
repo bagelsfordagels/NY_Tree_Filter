adb3bcd6b00f1bd342781fa9d8347394a34d7750
bagelsfordagels <benurgh@gmail.com>
2026-02-06 13:09:46 -0500

    added script to account for null values in tree table

--- addTreeID.py
+++ addTreeID.py
@@ -1,10 +1,14 @@
 import pandas as pd
+def NA(value):
+    if pd.isnull(value):
+        return "Uncertain"
+    return value
 fileName = input("Enter name of file (or stop to exit): ")
 
 df = pd.read_csv(f'./csv_files/{fileName}.csv', header=0);
 
 df.insert(0, "TreeID", range(1, len(df) + 1))
-
+df.iloc[1:,1:] = df.iloc[1:,1:].map(NA) # [row, clumn] 1: skip first look at rest
 print(df);
 
 df.to_csv(f'./bool_csv/{fileName}.csv',index=False);
